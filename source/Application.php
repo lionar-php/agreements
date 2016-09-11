1efaf6a994a12695f447cd3d0cdb3c127168e948
Aron Wouters <lionar@mail.com>
2016-09-11 23:44:37 +0200

    Added router agreement

--- source/Application.php
+++ source/Application.php
@@ -7,45 +7,45 @@ use Closure;
 interface Application
 {
 	/**
-	 * Bind an abstract type that becomes a shared instance
+	 * Bind an abstract type that becomes a shared instance.
 	 * 
-	 * @param  string  $abstract 	The abstract type to register
-	 * @param  Closure $concrete 	The concrete definition of the abstract type
+	 * @param  string  $abstract 	The abstract type to register.
+	 * @param  Closure $concrete 	The concrete definition of the abstract type.
 	 * @return void
 	 */
 	public function share ( $abstract, Closure $concrete );
 
 	/**
-	 * Bind an abstract type that must be resolved every single time
+	 * Bind an abstract type that must be resolved every single time.
 	 * 
-	 * @param  string  $abstract 	The abstract type to register
-	 * @param  Closure $concrete 	The concrete definition of the abstract type
+	 * @param  string  $abstract 	The abstract type to register.
+	 * @param  Closure $concrete 	The concrete definition of the abstract type.
 	 * @return void
 	 */
 	public function bind ( $abstract, Closure $concrete );
 
 	/**
-	 * Resolve an abstract type out of the application
+	 * Resolve an abstract type out of the application.
 	 * 
-	 * @param  string $abstract 	The abstract type to resolve
-	 * @return mixed           		The results of the abstract type
+	 * @param  string $abstract 	The abstract type to resolve.
+	 * @return mixed           		The results of the abstract type.
 	 */
 	public function make ( $abstract );
 
 	/**
-	 * Check if the application has an abstract type registered
+	 * Check if the application has an abstract type registered.
 	 * 
-	 * @param  string  $abstract 	The abstract type to check for registration
-	 * @return boolean           	True if registered false if not
+	 * @param  string  $abstract 	The abstract type to check for registration.
+	 * @return boolean           	True if registered false if not.
 	 */
 	public function has ( $abstract ) : bool;
 
 	/**
-	 * Resolve the results of a closure injecting needed dependencies
+	 * Resolve the results of a closure injecting needed dependencies.
 	 * 
-	 * @param  Closure $concrete 	The closure to resolve
-	 * @param  array   $payload  	Optional arguments to resolve the closure with
-	 * @return mixed            	The results of the resolved closure
+	 * @param  Closure $concrete 	The closure to resolve.
+	 * @param  array   $payload  	Optional arguments to resolve the closure with.
+	 * @return mixed            	The results of the resolved closure.
 	 */
 	public function call ( Closure $concrete, array $payload = array ( ) );
 }
---
+++ source/Routing/Router.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Agreed\Routing;
+
+interface Router
+{
+	/**
+	 * Adding a route under an action into the router.
+	 * 
+	 * @param string $route  	The route to add.
+	 * @param string $action 	The action to add under the route.
+	 */
+	public function add ( $route, $action );
+
+	/**
+	 * Determine if the router has the given route registered.
+	 * 
+	 * @param  string  $route 	The route to check for registration.	
+	 * @return boolean        	True if registered false if not.
+	 */
+	public function has ( $route ) : bool;
+
+	/**
+	 * Get the action registered under the given route.
+	 * 
+	 * @param  string $route 	The route to get an action from.
+	 * @return string        	The action that is registered under the given route.
+	 */
+	public function get ( $route ) : string;
+}
