680ba2e8b07d2469319b6075a97b11bce5f19e2b
Aron Wouters <lionar@mail.com>
2016-09-18 17:29:41 +0200

    Worked on Storage stack agreement

--- source/Storage/Stack.php
+++ source/Storage/Stack.php
@@ -4,13 +4,28 @@ namespace Agreed\Storage;
 
 interface Stack
 {
-	public function append ( $identifier, $entity );
+	/**
+	 * Set an identifier to a value.
+	 * 
+	 * @param  mixed $identifier 		The way to identify an entry.
+	 * @param  mixed $value     		The value to store.
+	 * @return void
+	 */
+	public function set ( $identifier, $value );
 
 	/**
-	 * Check wether this stack has the identifier given registered
+	 * Check wether this stack has the identifier given registered.
 	 * 
-	 * @param  string  $identifier 		The identifier to check for registration
-	 * @return boolean             		True if registered false if not
+	 * @param  string  $identifier 		The identifier to check for registration.
+	 * @return boolean             		True if registered false if not.
 	 */
 	public function has ( $identifier ) : bool;
+
+	/**
+	 * Get a value from the stack.
+	 * 
+	 * @param  mixed $identifier 		The identifier to get the value from.
+	 * @return mixed             		The value that was found with the identifier.
+	 */
+	public function get ( $identifier );
 }
